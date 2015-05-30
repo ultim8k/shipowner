4b1ccb2a496a9182e5dec4cbd61923ab3cc0da05
Konstantinos Kapenekakis <ultimatekonstantinos@gmail.com>
2015-05-30 20:24:09 +0300

    Adds footer, modal, actions, colors…

--- js/main.js
+++ js/main.js
@@ -1,4 +1,12 @@
-$(document).ready(function () {
+var openGameModal = function() {
+    var $modal = $('.js_modal');
+    $modal.removeClass('hidden');
+};
+var closeGameModal = function() {
+    var $modal = $('.js_modal');
+    $modal.addClass('hidden');
+};
+var prepareMap = function(){
     var map = L.map('sea-map', {
         zoomControl: false
     }).setView([37.675, 24.851], 7);
@@ -11,5 +19,18 @@ $(document).ready(function () {
     L.control.zoom({
         position: 'topright'
     }).addTo(map);
+};
 
+var actionsEvents = function() {
+    $('.js_ships_on_route').click(function () {
+        openGameModal();
+    });
+    $('.js_close_modal').click(function () {
+        closeGameModal();
+    });
+};
+
+$(document).ready(function () {
+    prepareMap();
+    actionsEvents();
 });
